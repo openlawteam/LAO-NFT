2279f937beb5795ff74b52a86dc037c4f901f9d4
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-09-15 15:01:04 -0400

    Update GAMMA.sol

--- contracts/GAMMA.sol
+++ contracts/GAMMA.sol
@@ -2,17 +2,17 @@ pragma solidity 0.5.17;
 
 contract GAMMA { // Γ - lo-code, lo-cost NFT
     uint256 public totalSupply;
-    string public name = "GAMMA";
+    string public name = "✨";
     string public symbol = "GAMMA";
-    event Approval(address indexed approver, address indexed spender, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
     mapping(address => uint256) public balanceOf;
     mapping(uint256 => address) public getApproved;
     mapping(uint256 => address) public ownerOf;
     mapping(uint256 => string) public tokenURI;
     mapping(bytes4 => bool) public supportsInterface; // eip-165 
     mapping(address => mapping(address => bool)) public isApprovedForAll;
+    event Approval(address indexed approver, address indexed spender, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
 
     constructor () public {
         balanceOf[msg.sender] = 1;
@@ -24,9 +24,6 @@ contract GAMMA { // Γ - lo-code, lo-cost NFT
         emit Transfer(address(0), msg.sender, 1);
     }
 
-    /************
-    TKN FUNCTIONS
-    ************/
     function approve(address spender, uint256 tokenId) external returns (bool) {
         address tokenOwner = ownerOf[tokenId];
         require(msg.sender == tokenOwner || isApprovedForAll[tokenOwner][msg.sender], "!owner/operator");
@@ -40,17 +37,6 @@ contract GAMMA { // Γ - lo-code, lo-cost NFT
         emit ApprovalForAll(msg.sender, operator, approved);
     }
 
-    function burn(uint256 tokenId) external {
-        address tokenOwner = ownerOf[tokenId];
-        require(msg.sender == tokenOwner || getApproved[tokenId] == msg.sender || isApprovedForAll[tokenOwner][msg.sender], "!owner/spender/approvedForAll");
-        balanceOf[tokenOwner] -= 1;
-        totalSupply -= 1; 
-        ownerOf[tokenId] = address(0);
-        getApproved[tokenId] = address(0);
-        tokenURI[tokenId] = "";
-        emit Transfer(msg.sender, address(0), tokenId);
-    }
-    
     function mint(address to, string calldata _tokenURI) external { // "open mint" - anyone can call new NFT to anyone
         totalSupply += 1;
         balanceOf[to] += 1;
