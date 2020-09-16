c3cbc3baa898e6f18f5ef169391a7976b79051dc
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-09-16 19:50:30 -0400

    Update GAMMA.sol
    add governance // enumeration

--- contracts/GAMMA.sol
+++ contracts/GAMMA.sol
@@ -1,55 +1,52 @@
 pragma solidity 0.5.17;
-
-contract GAMMA { // Γ - mv-NFT-mkt
+contract GAMMA { // Γ - mv-NFT-mkt - γ
+    address payable public dao = 0x057e820D740D5AAaFfa3c6De08C5c98d990dB00d;
     uint256 public constant GAMMA_MAX = 5772156649015328606065120900824024310421;
     uint256 public totalSupply;
-    string public name = "✨";
+    string public name = "GAMMA";
     string public symbol = "GAMMA";
-    
     mapping(address => uint256) public balanceOf;
     mapping(uint256 => address) public getApproved;
     mapping(uint256 => address) public ownerOf;
+    mapping(uint256 => uint256) public tokenByIndex;
     mapping(uint256 => string) public tokenURI;
     mapping(uint256 => Sale) public sale;
     mapping(bytes4 => bool) public supportsInterface; // eip-165 
     mapping(address => mapping(address => bool)) public isApprovedForAll;
-    
+    mapping(address => mapping(uint256 => uint256)) public tokenOfOwnerByIndex;
     event Approval(address indexed approver, address indexed spender, uint256 indexed tokenId);
     event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    
+    event UpdateSale(uint256 indexed ethPrice, uint256 indexed tokenId, bool forSale);
     struct Sale {
         uint256 ethPrice;
         bool forSale;
     }
-    
     constructor () public {
-        balanceOf[msg.sender] = 1;
-        totalSupply = 1;
-        ownerOf[1] = msg.sender;
-        tokenURI[1] = "γ";
         supportsInterface[0x80ac58cd] = true; // ERC721 
         supportsInterface[0x5b5e139f] = true; // METADATA
-        emit Transfer(address(0), msg.sender, 1);
+        supportsInterface[0x780e9d63] = true; // ENUMERABLE
     }
-    
     function approve(address spender, uint256 tokenId) external {
         address owner = ownerOf[tokenId];
         require(msg.sender == owner || isApprovedForAll[owner][msg.sender], "!owner/operator");
         getApproved[tokenId] = spender;
         emit Approval(msg.sender, spender, tokenId); 
     }
-    
-    function mint(string calldata _tokenURI) external { 
+    function mint(uint256 ethPrice, string calldata _tokenURI, bool forSale) external { 
         balanceOf[msg.sender]++;
         totalSupply++;
         require(totalSupply <= GAMMA_MAX, "maxed");
         uint256 tokenId = totalSupply;
         ownerOf[tokenId] = msg.sender;
+        tokenByIndex[tokenId - 1] = tokenId;
+        sale[tokenId].ethPrice = ethPrice;
+        sale[tokenId].forSale = forSale;
         tokenURI[tokenId] = _tokenURI;
+        tokenOfOwnerByIndex[msg.sender][tokenId - 1] = tokenId;
         emit Transfer(address(0), msg.sender, tokenId); 
+        emit UpdateSale(ethPrice, tokenId, forSale);
     }
-    
     function purchase(uint256 tokenId) payable external {
         require(msg.value == sale[tokenId].ethPrice, "!ethPrice");
         require(sale[tokenId].forSale, "!forSale");
@@ -58,44 +55,46 @@ contract GAMMA { // Γ - mv-NFT-mkt
         require(success, "!transfer");
         _transfer(owner, msg.sender, tokenId);
     }
-    
     function setApprovalForAll(address operator, bool approved) external {
         isApprovedForAll[msg.sender][operator] = approved;
         emit ApprovalForAll(msg.sender, operator, approved);
     }
-    
     function _transfer(address from, address to, uint256 tokenId) internal {
         balanceOf[from]--; 
         balanceOf[to]++; 
         getApproved[tokenId] = address(0);
         ownerOf[tokenId] = to;
         sale[tokenId].forSale = false;
+        tokenOfOwnerByIndex[from][tokenId - 1] = 0;
+        tokenOfOwnerByIndex[to][tokenId - 1] = tokenId;
         emit Transfer(from, to, tokenId); 
     }
-    
     function transfer(address to, uint256 tokenId) external {
         require(msg.sender == ownerOf[tokenId], "!owner");
         _transfer(msg.sender, to, tokenId);
     }
-    
     function transferBatch(address[] calldata to, uint256[] calldata tokenId) external {
         require(to.length == tokenId.length, "!to/tokenId");
-        
         for (uint256 i = 0; i < to.length; i++) {
             require(msg.sender == ownerOf[tokenId[i]], "!owner");
             _transfer(msg.sender, to[i], tokenId[i]);
         }
     }
-    
     function transferFrom(address from, address to, uint256 tokenId) external {
         address owner = ownerOf[tokenId];
         require(msg.sender == owner || getApproved[tokenId] == msg.sender || isApprovedForAll[owner][msg.sender], "!owner/spender/operator");
         _transfer(from, to, tokenId);
     }
-    
-    function updateSale(uint256 ethPrice, uint256 tokenId, bool forSale) external {
+    function updateDao(address payable _dao) external {
+        require(msg.sender == dao, "!dao");
+        dao = _dao;
+    }
+    function updateSale(uint256 ethPrice, uint256 tokenId, bool forSale) payable external {
         require(msg.sender == ownerOf[tokenId], "!owner");
         sale[tokenId].ethPrice = ethPrice;
         sale[tokenId].forSale = forSale;
+        (bool success, ) = dao.call.value(msg.value)("");
+        require(success, "!transfer");
+        emit UpdateSale(ethPrice, tokenId, forSale);
     }
 }
