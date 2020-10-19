9cad60b3a5f6b2e3342459083dd37a29fb94d21f
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-10-19 11:44:17 -0400

    Update NFTWrap.sol
    use more generalized transferFrom

--- contracts/NFTWrap.sol
+++ contracts/NFTWrap.sol
@@ -4,8 +4,7 @@
 ██╔██╗ ██║█████╗     ██║          
 ██║╚██╗██║██╔══╝     ██║          
 ██║ ╚████║██║        ██║          
-╚═╝  ╚═══╝╚═╝        ╚═╝          
-                                  
+╚═╝  ╚═══╝╚═╝        ╚═╝                                        
 ██╗    ██╗██████╗  █████╗ ██████╗ 
 ██║    ██║██╔══██╗██╔══██╗██╔══██╗
 ██║ █╗ ██║██████╔╝███████║██████╔╝
@@ -42,16 +41,8 @@ library SafeMath {
 /**
  * @dev Required interface of an ERC721 compliant contract.
  */
-interface IERC721 {
-    function balanceOf(address owner) external view returns (uint256 balance);
-    function ownerOf(uint256 tokenId) external view returns (address owner);
-    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+interface IERC721transferFrom {
     function transferFrom(address from, address to, uint256 tokenId) external;
-    function approve(address to, uint256 tokenId) external;
-    function getApproved(uint256 tokenId) external view returns (address operator);
-    function setApprovalForAll(address operator, bool _approved) external;
-    function isApprovedForAll(address owner, address operator) external view returns (bool);
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
 }
 
 contract NFTWrap {
@@ -316,7 +307,7 @@ contract NFTWrapper is CloneFactory {
             _forSale, 
             _transferable);
         
-        IERC721(nftToWrap).safeTransferFrom(msg.sender, address(nftWrap), nftToWrapId);
+        IERC721transferFrom(nftToWrap).transferFrom(msg.sender, address(nftWrap), nftToWrapId);
         emit LaunchNFTWrap(address(nftWrap), _manager, nftToWrap, _resolver, nftToWrapId, _forSale);
     }
 }
